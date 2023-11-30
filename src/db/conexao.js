46b24b82918575e90aad2bb2f9f18e7325115e2a
lith <lith@pop-os.localdomain>
2023-11-30 18:26:18 -0300

    criada conexão knex com o banco de dados remoto (elefantesql)

--- src/db/conexao.js
+++ src/db/conexao.js
@@ -1,1 +1,10 @@
-a
+module.exports = require('knex')({
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PSWD,
+        database: process.env.DB_NAME
+    }
+})
