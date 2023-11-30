21daf624137902901f4dda21ad4bc57e80e6b232
lith <lith@pop-os.localdomain>
2023-11-30 17:53:45 -0300

    Primeiro commit | up da branch lith | criado o arquivo .env | criado arquivo dump.sql

--- src/db/dump.sql
+++ src/db/dump.sql
@@ -1,1 +1,22 @@
-a
+create table usuarios(
+ id serial primary key,
+  nome text not null,
+  email varchar(200) not null unique,
+  senha text not null
+);
+
+create table categorias(
+ id serial primary key,
+  descricao text not null
+);
+
+insert into categorias(descricao) values
+('Informática'),
+('Celulares'),
+('Beleza e Perfumaria'),
+('Mercado'),
+('Livros e Papelaria'),
+('Brinquedos'),
+('Moda'),
+('Bebê'),
+('Games');
