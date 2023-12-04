e5a38e74f3ad0279c459a5670767ab4b6c0a1acf
lith <lith@pop-os.localdomain>
2023-12-04 14:31:53 -0300

    Corrigido resposta do controller listarCategorias e resposta em caso de erro

--- src/controllers/others.js
+++ src/controllers/others.js
@@ -4,11 +4,11 @@ const knex = require("../db/conexao");
 const listarCategoria = async (req,res) =>{
   
     try{
-      const categorias = await knex("categorias").select("descricao");
+      const categorias = await knex("categorias").select("*");
       
       return res.status(200).json(categorias);
     }catch(error){
-      console.log(error.message)
+      return res.status(500).json({'mensagem':"Erro interno do servidor"});
     }
   }
   
