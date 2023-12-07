cdf8a9718257cbbfb903e2192c61e2f2f2007a75
Arthur <arthurd0605@gmail.com>
2023-12-07 01:41:46 -0300

    feat adicionando o listar-clientes

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -2,32 +2,38 @@ const express = require("express");
 const controllersUsuario = require("../controllers/usuario-controller");
 const schemaUsuario = require("../utils/validarCampos");
 const middlewaresUsuario = require("../middlewares/middlewares");
-const {listarCategoria} = require("../controllers/categoria-controller");
-const validarLogin = require("../utils/validarCamposLogin")
-
+const { listarCategoria } = require("../controllers/categoria-controller");
+const validarLogin = require("../utils/validarCamposLogin");
+const controllerCliente = require("../controllers/cliente-controller");
 
 const rotas = express.Router();
 
 rotas.post(
-    "/usuario",
-    middlewaresUsuario.validarCampos(schemaUsuario),
-    middlewaresUsuario.verificarEmailUsuario, controllersUsuario.cadastrarUsuario
+  "/usuario",
+  middlewaresUsuario.validarCampos(schemaUsuario),
+  middlewaresUsuario.verificarEmailUsuario,
+  controllersUsuario.cadastrarUsuario
 );
 
-rotas.post("/login",middlewaresUsuario.validarCampos(validarLogin),
-    controllersUsuario.loginDoUsuario);
+rotas.post(
+  "/login",
+  middlewaresUsuario.validarCampos(validarLogin),
+  controllersUsuario.loginDoUsuario
+);
 
 rotas.get("/categorias", listarCategoria);
 
 rotas.use(middlewaresUsuario.validarToken); // route middleware //colocar todos end points que precisam de token abaixo dessa funçao.
 
 rotas.put(
-    "/usuario",
-    middlewaresUsuario.validarCampos(schemaUsuario),
-    middlewaresUsuario.verificarEmailUsuario,
-    controllersUsuario.editarUsuario
+  "/usuario",
+  middlewaresUsuario.validarCampos(schemaUsuario),
+  middlewaresUsuario.verificarEmailUsuario,
+  controllersUsuario.editarUsuario
 );
 
-rotas.get("/usuario", controllersUsuario.detalharUsuario)
+rotas.get("/usuario", controllersUsuario.detalharUsuario);
+
+rotas.get("/cliente", controllerCliente.listarClientes);
 
 module.exports = rotas;
