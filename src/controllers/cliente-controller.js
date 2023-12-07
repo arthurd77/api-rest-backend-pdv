351038ed8629d831a1ce9de4d26a950aefc8db7b
Arthur <arthurd0605@gmail.com>
2023-12-07 02:06:11 -0300

    removendo variavel indesejada

--- src/controllers/cliente-controller.js
+++ src/controllers/cliente-controller.js
@@ -1,8 +1,6 @@
 const knex = require("../db/conexao");
 
 const listarClientes = async (req, res) => {
-  const usuarioId = req.usuarioId;
-
   const cliente = await knex("clientes").select("id", "nome", "email");
 
   return res.status(200).json(cliente);
